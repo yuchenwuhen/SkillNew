34c447395721a528525c12efd2093d54798e3ff4
vimrzhang <vimrzhang@tencent.com>
2020-09-15 20:44:03 +0800

    1

---
+++ SkillEditor/SkillEditor/Assets/Editor/SkillPlayerEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SkillPlayer))]
+public class SkillPlayerEditor_Inspector : Editor
+{
+    public SkillPlayer skillPlayer = null;
+
+    private AniPlayer m_aniPlayer;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (skillPlayer == null)
+        {
+            skillPlayer = target as SkillPlayer;
+        }
+
+        if (m_aniPlayer == null)
+        {
+            m_aniPlayer = this.skillPlayer.transform.GetComponent<AniPlayer>();
+        }
+
+        if (this.skillPlayer.GetComponent<AniPlayer>() == null)
+        {
+            EditorGUILayout.HelpBox("添加AniPlayer", MessageType.Error);
+            return;
+        }
+
+        if (skillPlayer.skills == null)
+        {
+            if (GUILayout.Button("创建skill数据"))
+            {
+                var skill = ScriptableObject.CreateInstance<Skills>();
+
+                var cp = AssetDatabase.GetAssetPath(m_aniPlayer.clips[0].GetInstanceID());
+
+
+            }
+        }
+    }
+}
---
+++ SkillEditor/SkillEditor/Assets/Scripts/Core/Common/SkillPlayer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPlayer : MonoBehaviour
+{
+    public Skills skills = null;
+}
